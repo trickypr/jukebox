1b2fbb3ff5b8f5285c88988aa6f24dd9f3d138be
trickypr <git@trickypr.com>
2024-10-15 13:49:53 +1100

    Fix websocket resolution

--- Dockerfile
+++ Dockerfile
@@ -33,7 +33,7 @@ COPY ./ui/ ./ui/
 # Move production constants over
 RUN rm ./ui/Const.elm && mv ./ui/Const.prod.elm ./ui/Const.elm
 # NOTE: We do not regenerate bindings and hope that the developers did that whilst developing
-ENV SERVER=""
+ENV VITE_SERVER="relative"
 RUN pnpm vite build
 
 FROM alpine:latest
--- ui/pages/main.js
+++ ui/pages/main.js
@@ -4,9 +4,15 @@ const app = Elm.Main.init({
   node: document.getElementById("root"),
 });
 
-const socket = new WebSocket(
-  `ws://${import.meta.env.SERVER ?? "localhost:3000/"}api/live`,
-);
+let websocket_path = "ws://localhost:3000/";
+
+if (import.meta.env.VITE_SERVER === "relative") {
+  websocket_path = `wss://${window.location.host}/`;
+} else if (import.meta.env.VITE_SERVER) {
+  websocket_path = import.meta.env.VITE_SERVER;
+}
+
+const socket = new WebSocket(`${websocket_path}api/live`);
 
 socket.addEventListener("message", ({ data }) => {
   console.log("message", data);
